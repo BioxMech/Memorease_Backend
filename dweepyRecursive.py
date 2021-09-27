de9648aefd1b4bdd7a7528f250888c30c59613c1
Jason <70564653+BioxMech@users.noreply.github.com>
2021-09-27 20:59:58 +0800

    Update dweepyRecursive.py

--- dweepyRecursive.py
+++ dweepyRecursive.py
@@ -1,10 +1,25 @@
-def cycle_from_dweet():
-        try:
-            for dweet in dweepy.listen_for_dweets_from('memorease'):
-                print("Listening to updates from dweet thing 'memorease'...")
-                print(dweet)
+import dweepy
 
-        except requests.exceptions.ConnectionError as e:
-            print(e.response)
-            print("Connection closed by dweet, restarting:")
-            cycle_from_dweet()
+def cycle_from_dweet():
+  try:
+    for dweet in dweepy.listen_for_dweets_from('memorease'):
+      print("Listening to updates from dweet thing 'memorease'...")
+      print(dweet)
+      location = dweet["content"]["location"] 
+      execution(location)
+      raise ConnectionError()        
+
+
+  except requests.exceptions.ConnectionError as e:
+    print(e.response)
+    print("Connection closed by dweet, restarting:")
+    cycle_from_dweet()
+
+def execution(location):
+  if location == "stop":
+    ...
+  else:
+    ...
+  cycle_from_dweet()
+
+cycle_from_dweet()
